bc8fa686c9f32607b04b398b3003ff68a77862db
Jonas <jonas.mpn@gmail.com>
2023-07-29 21:36:25 +0200

    Interim example

--- src/main.py
+++ src/main.py
@@ -1,83 +1,73 @@
-from BEM import solve_condition, steady_reference
-import matplotlib.pyplot as plt
-from data_handler import Rotor, Flow, Simulation, Results
-import numpy as np
+from BEM import calculate_case
+from data_handler import Rotor, Flow, Simulation, Results, plot_case_results
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-
+# change rotor parameters here
 rotor = Rotor()
+# change flow parameters here
 flow = Flow(rotor)
+# change airfoil here (Root airfoil: alpha, Cl, Cd, Cm)
 airfoil = {'DU 95-W-180': pd.read_excel("../data/input/DU95W180.xlsx", skiprows=3)}
-simulation = Simulation(model="Steady")
+# change simulation parameters here
+simulation = Simulation(model="Steady", t_max=30, dt=0.5)
 
-reference = steady_reference(rotor, airfoil, flow, simulation, ("pitch", np.deg2rad(np.arange(-10, 10))))
-plt.plot(reference["pitch"], reference["CT"])
-plt.plot(reference["pitch"], reference["CP"])
-plt.show()
+calc_and_plot = {
+		"case_A_1": True,
+		"case_A_2": True,
+		"case_B_1": True,
+		"case_B_2": True
+}
 
+if calc_and_plot["case_A_1"]:
+	results = calculate_case(rotor, flow, airfoil, simulation,
+	                         change_param="CT_steady",
+	                         change_type="step",
+	                         change_values=(0.5, 0.9))
+	fig, ax = plt.subplots(2, 1)
+	for result in results:
+		ax[0].plot(result.simulation.time, result.CT, label=result.simulation.model)
+		ax[1].plot(result.simulation.time, np.mean(result.a, axis=1), label=result.simulation.model)
+	plt.legend()
+	plt.show()
+	
 
+if calc_and_plot["case_A_2"]:
+	simulation.reset()
+	results = calculate_case(rotor, flow, airfoil, simulation,
+	                         change_param="CT_steady",
+	                         change_type="sine",
+	                         change_values=(0.5, 0.5, 0.3/(2*np.pi*rotor.R)*flow.V0))
+	fig, ax = plt.subplots(2, 1)
+	for result in results:
+		ax[0].plot(result.simulation.time, result.CT, label=result.simulation.model)
+		ax[1].plot(result.simulation.time, np.mean(result.a, axis=1), label=result.simulation.model)
+	plt.legend()
+	plt.show()
 
+if calc_and_plot["case_B_1"]:
+	simulation.reset()
+	results = calculate_case(rotor, flow, airfoil, simulation,
+	                         change_param="U_inf",
+	                         change_type="step",
+	                         change_values=(1, 1.5))
+	fig, ax = plt.subplots(2, 1)
+	for result in results:
+		ax[0].plot(result.simulation.time, result.CT, label=result.simulation.model)
+		ax[1].plot(result.simulation.time, np.mean(result.a, axis=1), label=result.simulation.model)
+	plt.legend()
+	plt.show()
 
-
-
-# # change rotor parameters here
-# rotor = Rotor()
-# # change flow parameters here
-# flow = Flow(rotor)
-# # change airfoil here (Root airfoil: alpha, Cl, Cd, Cm)
-# airfoil = {'DU 95-W-180': pd.read_excel('DU95W180.xlsx', skiprows=3)}
-# # change simulation parameters here
-# simulation = Simulation(model="Steady")
-# # result container does not need changes in the initialisation
-# results = Results(rotor, simulation)
-#
-# CT0 = 0.3
-# CT1 = 0.6
-
-
-
-
-#
-# # find twist
-# reference = generate_reference_curve(rotor, airfoil, flow, simulation, results)
-#
-# f = interpolate.interp1d(reference['CT'], reference['beta'])
-# beta0, beta1 = f([CT0, CT1])
-#
-# # beta0 = np.interp(CT0, reference['CT'], reference['beta'])
-# # beta1 = np.interp(CT1, reference['CT'], reference['beta'])
-#
-# # plt.figure()
-# # plt.plot( reference["CT"], reference["beta"])
-# # plt.scatter([CT0, CT1],[beta0, beta1])
-#
-#
-# beta = np.zeros(np.shape(simulation['time']))+beta0  # we initialize the array of thrust coefficient,
-# # setting all initial values at Ct0
-#
-# beta[len(beta)//2:] = beta1
-#
-# models = ["Steady", "OYE", "LM", "PP"]
-# fig, ax1 = plt.subplots()
-# ax2 = ax1.twinx()
-# linestyles = ["dashed", "dotted", "dashdot", "solid"]
-# for i, m in enumerate(models):
-# 	CT_array = np.zeros(len(simulation['time']))
-# 	a_array = np.zeros(len(simulation['time']))
-# 	simulation['current_index'] = 0
-# 	for j, b in enumerate(beta):
-# 		simulation['model'] = m
-# 		rotor['theta_pitch'] = b
-# 		curr_res = function_BEM(rotor, airfoil, flow, simulation, results)
-# 		for k, key in enumerate(results):
-# 			results[key][j] = curr_res[k]
-#
-# 	ax1.plot(simulation['time'], results['CT'], color='g', ls=linestyles[i], label=m)
-# 	# print(results['CT'])
-# 	ax2.plot(simulation['time'], np.mean(results['a'], axis=1), color='b', ls=linestyles[i], label=m)
-#
-# ax1.set_xlabel('Time')
-# ax1.set_ylabel('CT', color='g')
-# ax2.set_ylabel('a', color='b')
-# plt.legend()
-# plt.show()
+if calc_and_plot["case_B_1"]:
+	simulation.reset()
+	results = calculate_case(rotor, flow, airfoil, simulation,
+	                         change_param="CT_steady",
+	                         change_type="sine",
+	                         change_values=(1, 0.5, 0.3/(2*np.pi*rotor.R)*flow.V0))
+	fig, ax = plt.subplots(2, 1)
+	for result in results:
+		ax[0].plot(result.simulation.time, result.CT, label=result.simulation.model)
+		ax[1].plot(result.simulation.time, np.mean(result.a, axis=1), label=result.simulation.model)
+	plt.legend()
+	plt.show()
