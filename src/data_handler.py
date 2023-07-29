a85325600dcc86f8dd4145a3809107c15d7b476e
Jonas <jonas.mpn@gmail.com>
2023-07-29 21:35:59 +0200

    Started working on the automated plotting of the case results

--- src/data_handler.py
+++ src/data_handler.py
@@ -1,6 +1,10 @@
 from dataclasses import dataclass, field
+
+import matplotlib.pyplot as plt
 import numpy as np
 from copy import copy
+from helper_functions import Helper
+helper = Helper()
 
 
 @dataclass
@@ -97,9 +101,14 @@ class Simulation:
 	t_max: float = 30
 	
 	time: np.ndarray = field(init=False)
+	actual_t_max: np.float = field(init=False)
 	
 	def __post_init__(self):
 		self.time = np.arange(0, self.t_max, self.dt)
+		self.actual_t_max = self.time[-1]
+	
+	def reset(self):
+		self.current_index = 0
 	
 	def __getitem__(self, item):
 		return self.__dict__[item]
@@ -128,7 +137,10 @@ class Results:
 	ap: np.ndarray = field(init=False)
 	f: np.ndarray = field(init=False)
 	Ct: np.ndarray = field(init=False)
+	Cq: np.ndarray = field(init=False)
 	v_int: np.ndarray = field(init=False)
+	alpha: np.ndarray = field(init=False)
+	phi: np.ndarray = field(init=False)
 	
 	def __post_init__(self):
 		self.skip = ("skip", "rotor", "simulation")
@@ -143,8 +155,11 @@ class Results:
 		self.ap = np.zeros((n_time_steps, n_blade_elements))
 		self.f = np.zeros((n_time_steps, n_blade_elements))
 		self.Ct = np.zeros((n_time_steps, n_blade_elements))
+		self.Cq = np.zeros((n_time_steps, n_blade_elements))
 		self.v_int = np.zeros((n_time_steps, n_blade_elements))
-	
+		self.alpha = np.zeros((n_time_steps, n_blade_elements))
+		self.phi = np.zeros((n_time_steps, n_blade_elements))
+		
 	def __getitem__(self, item):
 		return self.__dict__[item]
 	
@@ -156,4 +171,49 @@ class Results:
 		
 	def __iter__(self):
 		return iter(tuple([param for param in self.__dict__.keys() if param not in self.skip]))
+	
+
+def plot_case_results(results: list[Results], save_dir: str, change_param: str, change_type: str, change_values: tuple):
+	helper.create_dir(save_dir)
+	fig_Ct, ax_Ct = plt.subplots()
+	fig_Cq, ax_Cq = plt.subplots()
+	fig_a, ax_a = plt.subplots()
+	fig_ap, ax_ap = plt.subplots()
+	fig_alpha, ax_alpha = plt.subplots()
+	fig_phi, ax_phi = plt.subplots()
+	
+	n_elements = results[0].rotor.n_r
+	i_middle_element = np.floor(n_elements/2)
+	i_elements = np.array([0, i_middle_element, n_elements])
+	
+	for result in results:
+		for i_element in i_elements:
+			ax_Ct.plot(result.simulation.time, result.Ct[:, i_element], label=result.simulation.model)
+			ax_Cq.plot(result.simulation.time, result.Cq[:, i_element], label=result.simulation.model)
+			ax_a.plot(result.simulation.time, result.a[:, i_element], label=result.simulation.model)
+			ax_ap.plot(result.simulation.time, result.ap[:, i_element], label=result.simulation.model)
+			ax_alpha.plot(result.simulation.time, result.alpha[:, i_element], label=result.simulation.model)
+			ax_phi.plot(result.simulation.time, result.phi[:, i_element], label=result.simulation.model)
+			
+	axes = [ax_Ct, ax_Cq, ax_a, ax_ap, ax_alpha, ax_phi]
+	figs = [fig_Ct, fig_Cq, fig_a, fig_ap, fig_alpha, fig_phi]
+	params = ["Ct", "Cq", "a", "ap", "alpha", "phi"]
+	file_name_base = change_param+"_"+change_type+"_"+str(change_values)+".png"
+	x_label = "time (s)"
+	y_labels = ["local thrust coefficient (-)",
+	           "local torque coefficient (-)",
+	           "local axial induction (-)",
+	           "local tangential induction (-)",
+	           "local angle of attack (°)",
+	           "local inflow angle (°)"]
+	
+	for param, fig, ax, y_label in zip(params, figs, axes, y_labels):
+		file = save_dir + f"/{param}_{file_name_base}"
+		helper.handle_axis(ax, x_label=x_label, y_label=y_label)
+		helper.handle_figure(fig, save_to=file)
+	
+	
+	
+	
+	
 	
