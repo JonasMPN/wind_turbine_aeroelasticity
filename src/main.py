29274fced5b544a3d3c733328ad736dec79a66ef
Jonas <jonas.mpn@gmail.com>
2023-07-30 10:20:39 +0200

    Updated to final state

--- src/main.py
+++ src/main.py
@@ -10,64 +10,53 @@ rotor = Rotor()
 flow = Flow(rotor)
 # change airfoil here (Root airfoil: alpha, Cl, Cd, Cm)
 airfoil = {'DU 95-W-180': pd.read_excel("../data/input/DU95W180.xlsx", skiprows=3)}
-# change simulation parameters here
-simulation = Simulation(model="Steady", t_max=30, dt=0.5)
+
+
+# turn off the print statements of the 'calculate_case' by setting Simulation(verbose=False).
 
 calc_and_plot = {
-		"case_A_1": True,
+		"case_A_1": False,
 		"case_A_2": True,
-		"case_B_1": True,
-		"case_B_2": True
+		"case_B_1": False,
+		"case_B_2": False
 }
 
 if calc_and_plot["case_A_1"]:
-	results = calculate_case(rotor, flow, airfoil, simulation,
-	                         change_param="CT_steady",
-	                         change_type="step",
-	                         change_values=(0.5, 0.9))
-	fig, ax = plt.subplots(2, 1)
-	for result in results:
-		ax[0].plot(result.simulation.time, result.CT, label=result.simulation.model)
-		ax[1].plot(result.simulation.time, np.mean(result.a, axis=1), label=result.simulation.model)
-	plt.legend()
-	plt.show()
+	print("Working on case A.1")
+	simulation = Simulation(model="Steady", t_max=5, dt=0.1)
+	change_param = "CT_steady"
+	change_type = "step"
+	change_values = (0.5, 0.9)
+	results = calculate_case(rotor, flow, airfoil, simulation, change_param, change_type, change_values)
+	plot_case_results(results, "../data/results", change_param, change_type, change_values)
 	
 
 if calc_and_plot["case_A_2"]:
-	simulation.reset()
-	results = calculate_case(rotor, flow, airfoil, simulation,
-	                         change_param="CT_steady",
-	                         change_type="sine",
-	                         change_values=(0.5, 0.5, 0.3/(2*np.pi*rotor.R)*flow.V0))
-	fig, ax = plt.subplots(2, 1)
-	for result in results:
-		ax[0].plot(result.simulation.time, result.CT, label=result.simulation.model)
-		ax[1].plot(result.simulation.time, np.mean(result.a, axis=1), label=result.simulation.model)
-	plt.legend()
-	plt.show()
+	print("Working on case A.2")
+	omega = 0.3/(2*np.pi*rotor.R)*flow.V0
+	simulation = Simulation(model="Steady", t_max=1/omega, dt=0.6)
+	change_param = "CT_steady"
+	change_type = "sine"
+	change_values = (0.5, 0.5, omega)
+	results = calculate_case(rotor, flow, airfoil, simulation, change_param, change_type, change_values)
+	plot_case_results(results, "../data/results", change_param, change_type, change_values)
 
 if calc_and_plot["case_B_1"]:
-	simulation.reset()
-	results = calculate_case(rotor, flow, airfoil, simulation,
-	                         change_param="U_inf",
-	                         change_type="step",
-	                         change_values=(1, 1.5))
-	fig, ax = plt.subplots(2, 1)
-	for result in results:
-		ax[0].plot(result.simulation.time, result.CT, label=result.simulation.model)
-		ax[1].plot(result.simulation.time, np.mean(result.a, axis=1), label=result.simulation.model)
-	plt.legend()
-	plt.show()
+	print("Working on case B.1")
+	simulation = Simulation(model="Steady", t_max=5, dt=0.05)
+	change_param = "U_inf"
+	change_type = "step"
+	change_values = (1, 1.5)
+	results = calculate_case(rotor, flow, airfoil, simulation, change_param, change_type, change_values)
+	plot_case_results(results, "../data/results", change_param, change_type, change_values)
 
 if calc_and_plot["case_B_1"]:
-	simulation.reset()
-	results = calculate_case(rotor, flow, airfoil, simulation,
-	                         change_param="CT_steady",
-	                         change_type="sine",
-	                         change_values=(1, 0.5, 0.3/(2*np.pi*rotor.R)*flow.V0))
-	fig, ax = plt.subplots(2, 1)
-	for result in results:
-		ax[0].plot(result.simulation.time, result.CT, label=result.simulation.model)
-		ax[1].plot(result.simulation.time, np.mean(result.a, axis=1), label=result.simulation.model)
-	plt.legend()
-	plt.show()
+	print("Working on case B.2")
+	omega = 0.3/(2*np.pi*rotor.R)*flow.V0
+	simulation = Simulation(model="Steady", t_max=1/omega, dt=0.7)
+	change_param = "CT_steady"
+	change_type = "sine"
+	change_values = (1, 0.5, omega)
+	results = calculate_case(rotor, flow, airfoil, simulation, change_param, change_type, change_values)
+	plot_case_results(results, "../data/results", change_param, change_type, change_values)
+
